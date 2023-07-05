425f3fcac32233e25a3b396b66a55db690a4e2be
karanvirsingh011998 <karanvir011998@gmail.com>
2023-07-05 12:17:40 +0530

    Product List Frontend

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./Header.css";
+
+const Header = (props) => {
+  return (
+    <header className="header">
+      <h1>Product Shop</h1>
+    </header>
+  );
+};
+
+export default Header;
